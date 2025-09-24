73b61b09f555a0405cfc79496bdb27b0ff1b327c
Yifan Zhao <yzhao257@uic.edu>
2025-09-25 00:07:23 +0800

    1

--- WEEKLYEXERCISE4.R
+++ WEEKLYEXERCISE4.R
@@ -1,1 +1,16 @@
 system("git branch --show-current")
+olympics <- read_csv("Olympics.csv")
+
+# (a) 新变量 total.medals
+olympics <- olympics %>%
+  mutate(total.medals = gold + silver + bronze)
+
+# (b) 每个国家金牌总数
+gold_by_country <- olympics %>%
+  group_by(country) %>%
+  summarize(total_gold = sum(gold, na.rm = TRUE))
+
+# (c) 每年总奖牌数
+medals_by_year <- olympics %>%
+  group_by(year) %>%
+  summarize(total_medals = sum(total.medals, na.rm = TRUE))
